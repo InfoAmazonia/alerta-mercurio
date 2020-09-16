94b7f688eebbad5736a503c0cb29b281ef001669
Allyson Souza <allyson@hastedesign.com.br>
2020-09-16 14:55:46 -0300

    Adicionado gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,24 @@ module.exports = {
 	},
 	plugins: [
 		 `gatsby-plugin-react-helmet`,
+		 {
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+			  name: "Infoamazonia - Mercury",
+			  short_name: "Infoamazonia",
+			  start_url: "/",
+			  background_color: "#FFFFFF",
+			  theme_color: "#73a229",
+			  // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+			  // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+			  display: "standalone",
+			  icon: "src/images/favicon.png", // This path is relative to the root of the site.
+			  // An optional attribute which provides support for CORS check.
+			  // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+			  // Any invalid keyword or empty string defaults to `anonymous`
+			  crossOrigin: `use-credentials`,
+			},
+		  },
 		 `gatsby-plugin-sass`,
 		 `gatsby-transformer-sharp`,
 		 `gatsby-plugin-sharp`,
